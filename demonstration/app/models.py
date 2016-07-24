029a8bacf8f6adb0a44581dea54a47bc34d9086b
Apply Hi Tech <dar2144@columbia.edu>
2016-07-24 11:46:43 -0400

    added Posts

--- demonstration/app/models.py
+++ demonstration/app/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,3 +9,18 @@ from django.db import models
 #TO DO: 
 
 #CREATE A USER WITH THE GITHUB DATA.
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(
+		blank=True, null=True)
+
+	def publish(self):
+		self.published_date=timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
